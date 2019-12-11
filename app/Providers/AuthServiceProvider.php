08746c2b07c71bab8ef7ec84c4561b73fbe8d201
yingjhen <jhenlin0220@gmail.com>
2019-12-11 11:58:52 +0800

    授權行為TaskController 的 destroy 方法

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -33,6 +33,21 @@ class AuthServiceProvider extends ServiceProvider
         //
     }
 
+    /**
+     * 移除給定的任務。
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+        return redirect('/tasks');
+
+        // 刪除該任務...
+    }
+
 
 
 
